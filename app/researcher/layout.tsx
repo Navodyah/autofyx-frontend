1dbae2b7ff91f6e51ba9bc3646a144b4ea61cb6b
navodyahansini <navodyahansini2002@gmail.com>
2026-04-06 03:07:57 +0530

    feat: add UserPreferenceOnboardingModal to ResearcherLayout

--- app/researcher/layout.tsx
+++ app/researcher/layout.tsx
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import { AuthProvider } from '@/lib/auth-context';
+import UserPreferenceOnboardingModal from '@/components/user-preference-onboarding-modal';
 
 export default function ResearcherLayout({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
       <div className="min-h-screen bg-background">
+        <UserPreferenceOnboardingModal />
         {children}
       </div>
     </AuthProvider>
